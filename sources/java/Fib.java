7cd5e7066c24376bd6e8951b82281813f6f6f441
Balancer <balancer@balancer.ru>
2013-10-18 13:24:01 +0400

    Добавлена Java.

---
+++ sources/java/Fib.java
@@ -0,0 +1,26 @@
+class Fib
+{
+    private final int _value;
+
+    Fib(int n) { _value = n; }
+
+    public int value()
+    {
+        if(_value <= 2)
+            return 1;
+
+        Fib f1 = new Fib(_value - 1);
+        Fib f2 = new Fib(_value - 2);
+
+        return f1.value() + f2.value();
+    }
+
+    public static void main(String[] argv)
+    {
+        for(int i=0; i<10; i++)
+        {
+            Fib x = new Fib(40);
+            System.out.println(x.value());
+        }
+    }
+}
