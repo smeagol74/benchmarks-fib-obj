1af2b8c4de1fc9bbb595c288a7d9022383fa4360
Balancer <balancer@balancer.ru>
2013-10-18 15:26:42 +0400

    ++

---
+++ sources/python/fib3.py
@@ -0,0 +1,15 @@
+class Fib:
+    def __init__(self, n):
+        self._value = n
+
+    def value(self):
+        if(self._value <= 2):
+            return 1
+
+        f1 = Fib(self._value - 1)
+        f2 = Fib(self._value - 2)
+
+        return f1.value() + f2.value()
+
+print(Fib(40).value())
+
